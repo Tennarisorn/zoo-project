aeb00b6a278c14fd79c19df6dc5d862596c449f1
Narisorn Sangnakara <narisorn.sang@mail.kmutt.ac.th>
2024-11-19 02:51:35 +0700

    Update test_zoo.py
    completed

--- test_zoo.py
+++ test_zoo.py
@@ -1,14 +1,24 @@
 import unittest
 from zoo import Zoo
 
-class TestZoo(unittest.TestCase):
+class TestZooTicketCalculator(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_negative_age(self):
+        self.assertIsNone(self.zoo.get_ticket_price(-10))
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(7), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(75), 100)
 
 if __name__ == '__main__':
     unittest.main()
